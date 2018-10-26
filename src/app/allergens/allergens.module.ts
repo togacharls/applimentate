e4ad0fdc069fff16b6e979c47416a9e22bec0bb8
Carlos Torres García <togacharls@gmail.com>
2018-10-26 13:06:19 +0200

    ionic generate page allergens

---
+++ src/app/allergens/allergens.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { AllergensPage } from './allergens.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AllergensPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [AllergensPage]
+})
+export class AllergensPageModule {}
---
+++ src/app/allergens/allergens.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-allergens',
+  templateUrl: './allergens.page.html',
+  styleUrls: ['./allergens.page.scss'],
+})
+export class AllergensPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
