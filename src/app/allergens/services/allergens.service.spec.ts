4c4b1c88c264cb7be8aa165c7bdb6f8f01e269a5
Jorge <jorgecampoy81@gmail.com>
2018-10-29 13:32:44 +0100

    Removed old path of gluten, improved allergens.service.spec.ts

--- src/app/allergens/services/allergens.service.spec.ts
+++ src/app/allergens/services/allergens.service.spec.ts
@@ -2,35 +2,27 @@ import { TestBed } from '@angular/core/testing';
 
 import { AllergensService } from './allergens.service';
 
-describe('AllergensService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe( 'AllergensService', () => {
+    beforeEach( () => TestBed.configureTestingModule( {} ) );
 
-  it('should be created', () => {
-    const service: AllergensService = TestBed.get(AllergensService);
-    expect(service).toBeTruthy();
-  });
-  it('should return a fourteen elements list', () => {
-      const service: AllergensService = TestBed.get(AllergensService),
-          allergensList = service.getList();
-      expect(allergensList.length).toBe(14);
-  });
-    it('should return the correct path and filename of each allergen icon', () => {
-        const service: AllergensService = TestBed.get(AllergensService),
+    it( 'should be created', () => {
+        const service: AllergensService = TestBed.get( AllergensService );
+        expect( service ).toBeTruthy();
+    } );
+    it( 'should return a fourteen elements list', () => {
+        const service: AllergensService = TestBed.get( AllergensService ),
+            allergensList = service.getList();
+        expect( allergensList.length ).toBe( 14 );
+    } );
+    it( 'should return the correct path and filename of each allergen icon', () => {
+        const service: AllergensService = TestBed.get( AllergensService );
+        const allergensName: string[] = [ 'altramuces', 'apio', 'cacahuetes', 'crustaceos', 'dioxido_de_azufre_y_sulfitos', 'frutos_secos',
+            'gluten', 'granos_de_sesamo', 'huevo', 'lacteos', 'moluscos', 'mostaza', 'pescado', 'soja' ],
             allergensList = service.getList(),
-            filterFunction = (allergenItem, expectedFilename) => allergenItem.imgSrc === `../../../assets/icon/${expectedFilename}.png`;
-        expect(allergensList.filter(item => filterFunction(item, 'altramuces')).length).toBe(1);
-        expect(allergensList.filter(item => filterFunction(item, 'apio')).length).toBe(1);
-        expect(allergensList.filter(item => filterFunction(item, 'cacahuetes')).length).toBe(1);
-        expect(allergensList.filter(item => filterFunction(item, 'crustaceos')).length).toBe(1);
-        expect(allergensList.filter(item => filterFunction(item, 'dioxido_de_azufre_y_sulfitos')).length).toBe(1);
-        expect(allergensList.filter(item => filterFunction(item, 'frutos_secos')).length).toBe(1);
-        expect(allergensList.filter(item => filterFunction(item, 'gluten')).length).toBe(1);
-        expect(allergensList.filter(item => filterFunction(item, 'granos_de_sesamo')).length).toBe(1);
-        expect(allergensList.filter(item => filterFunction(item, 'huevo')).length).toBe(1);
-        expect(allergensList.filter(item => filterFunction(item, 'lacteos')).length).toBe(1);
-        expect(allergensList.filter(item => filterFunction(item, 'moluscos')).length).toBe(1);
-        expect(allergensList.filter(item => filterFunction(item, 'mostaza')).length).toBe(1);
-        expect(allergensList.filter(item => filterFunction(item, 'pescado')).length).toBe(1);
-        expect(allergensList.filter(item => filterFunction(item, 'soja')).length).toBe(1);
-    });
-});
+
+            filterFunction = ( allergenItem, expectedFilename ) => allergenItem.imgSrc === `../../../assets/icon/${ expectedFilename }.png`;
+        for ( const allergen of allergensName ) {
+            expect( allergensList.filter( item => filterFunction( item, allergen ) ).length ).toBe( 1 );
+        }
+    } );
+} );
--- src/app/allergens/services/allergens.service.ts
+++ src/app/allergens/services/allergens.service.ts
@@ -1,43 +1,43 @@
 import { Injectable } from '@angular/core';
 import { AllergenInterface } from '../interfaces';
 
-@Injectable({
+@Injectable( {
   providedIn: 'root'
-})
+} )
 export class AllergensService {
 
   private srcPath = '../../../assets/icon/';
-  constructor() { }
+  constructor () { }
   getList(): AllergenInterface[] {
     const allergenList = [
-        { name: 'Altramuces', imgSrc: '' },
-        { name: 'Apio', imgSrc: '' },
-        { name: 'Cacahuetes', imgSrc: '' },
-        { name: 'Crustáceos', imgSrc: '' },
-        { name: 'Dióxido de azufre y sulfitos', imgSrc: '' },
-        { name: 'Frutos secos', imgSrc: '' },
-        { name: 'Gluten', imgSrc: this.srcPath + 'gluten.png' },
-        { name: 'Granos de sésamo', imgSrc: '' },
-        { name: 'Huevo', imgSrc: '' },
-        { name: 'Lácteos', imgSrc: '' },
-        { name: 'Moluscos', imgSrc: '' },
-        { name: 'Mostaza', imgSrc: '' },
-        { name: 'Pescado', imgSrc: '' },
-        { name: 'Soja', imgSrc: '' }
+      { name: 'Altramuces', imgSrc: '' },
+      { name: 'Apio', imgSrc: '' },
+      { name: 'Cacahuetes', imgSrc: '' },
+      { name: 'Crustáceos', imgSrc: '' },
+      { name: 'Dióxido de azufre y sulfitos', imgSrc: '' },
+      { name: 'Frutos secos', imgSrc: '' },
+      { name: 'Gluten', imgSrc: '' },
+      { name: 'Granos de sésamo', imgSrc: '' },
+      { name: 'Huevo', imgSrc: '' },
+      { name: 'Lácteos', imgSrc: '' },
+      { name: 'Moluscos', imgSrc: '' },
+      { name: 'Mostaza', imgSrc: '' },
+      { name: 'Pescado', imgSrc: '' },
+      { name: 'Soja', imgSrc: '' }
     ];
-    for (const allergen of allergenList) {
-        allergen.imgSrc = this.getIconFilename(allergen.name);
+    for ( const allergen of allergenList ) {
+      allergen.imgSrc = this.getIconFilename( allergen.name );
     }
     return allergenList;
   }
 
-  private getIconFilename(allergenName: string): string {
-      return this.srcPath + allergenName
-          .toLowerCase()
-          .normalize('NFD')
-          .replace(/[\u0300-\u036f]/g, '')
-          .split(' ')
-          .join('_')
-          + '.png';
+  private getIconFilename( allergenName: string ): string {
+    return this.srcPath + allergenName
+      .toLowerCase()
+      .normalize( 'NFD' )
+      .replace( /[\u0300-\u036f]/g, '' )
+      .split( ' ' )
+      .join( '_' )
+      + '.png';
   }
 }
