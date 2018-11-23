30fbdd3150014eb9d2cd4c94d1bbfd683191262c
Carlos Torres García <togacharls@gmail.com>
2018-11-23 16:39:02 +0100

    navigation to detail and allergens-detail title

--- src/app/allergens/pages/allergens-detail/allergens-detail.page.ts
+++ src/app/allergens/pages/allergens-detail/allergens-detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { AllergenInterface } from "../../interfaces";
 
 @Component({
   selector: 'app-allergens-details',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./allergens-detail.page.scss'],
 })
 export class AllergensDetailPage implements OnInit {
-
-  constructor() { }
+  allergen: AllergenInterface;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.allergen = {
+        imgSrc: null,
+        name: this.route.snapshot.params['id']
+    };
   }
 
 }
