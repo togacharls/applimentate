bbe301d40c4d1143fa30e811ea6194cde9418e77
Carlos Torres García <togacharls@gmail.com>
2019-01-06 16:27:07 +0100

    Fix 'ion-tab-button' is not a known element error

--- src/app/allergens/allergens.module.ts
+++ src/app/allergens/allergens.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
@@ -56,5 +56,6 @@ const routes: Routes = [
     AllergenFoodPage
   ],
   providers: [ AllergensService ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
 } )
 export class AllergensModule { }
