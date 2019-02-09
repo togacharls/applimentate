3c62f915beea902df693aaee9ddc3adab85a2aff
Jorge <jorgecampoy81@gmail.com>
2019-02-09 15:39:55 +0100

    Inputs ready

--- src/app/bmi/bmi.page.ts
+++ src/app/bmi/bmi.page.ts
@@ -1,26 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
+@Component( {
   selector: 'app-bmi',
   templateUrl: './bmi.page.html',
-  styleUrls: ['./bmi.page.scss'],
-})
+  styleUrls: [ './bmi.page.scss' ],
+} )
 export class BmiPage implements OnInit {
 
   height: number;
   weight: number;
   bmi: number;
 
-  constructor() { }
+  constructor () { }
 
   ngOnInit() {
-    this.height = null;
-    this.weight = null;
-    this.bmi = null;
+
   }
 
   calcBMI(): void {
-    this.bmi = (this.weight / ((this.height/100) * (this.height/100)));
+    this.bmi = ( this.weight / ( ( this.height / 100 ) * ( this.height / 100 ) ) );
   }
 
 }
