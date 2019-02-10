95c4a1ac7b64b943e48e2f0510e9aafcaa7c7547
Jorge <jorgecampoy81@gmail.com>
2019-02-10 21:17:19 +0100

    Added colors according weight; Added silhouettes according weight.

--- src/app/bmi/bmi.page.ts
+++ src/app/bmi/bmi.page.ts
@@ -11,29 +11,47 @@ export class BmiPage implements OnInit {
   weight: number;
   bmi = 0;
   sex = [ 'Man', 'Woman' ];
-  selecSex = this.sex[ 0 ];
+  selecSex: string = this.sex[ 0 ];
   typeWeight = [ 'slim', 'fit', 'normal', 'fat', 'fatest' ];
+  colorWeight = [ 'aqua', 'lime', 'yellow', 'orange', 'red' ];
   idxWeight = 0;
-  selecWeight = this.typeWeight[ this.idxWeight ];
+  selecWeight: string = this.typeWeight[ this.idxWeight ];
+
 
   constructor () { }
 
   ngOnInit() { }
 
-  changeSex() {
+  changeSex(): void {
     this.selecSex === this.sex[ 0 ]
       ? this.selecSex = this.sex[ 1 ]
       : this.selecSex = this.sex[ 0 ];
   }
 
-  changeImg() {
-    this.idxWeight++;
-    if ( this.idxWeight > 4 ) { this.idxWeight = 0; }
-    this.selecWeight = this.typeWeight[ this.idxWeight ];
-  }
-
-  calcBMI() {
-    const resultado = this.weight / ( ( this.height / 100 ) * ( this.height / 100 ) );
+  calcBMI(): void {
+    const resultado: number = this.weight / ( ( this.height / 100 ) * ( this.height / 100 ) );
+    switch ( true ) {
+      case ( resultado <= 18 ):
+        this.selecWeight = this.typeWeight[ 0 ];
+        this.idxWeight = 0;
+        break;
+      case ( resultado > 18 && resultado <= 24 ):
+        this.selecWeight = this.typeWeight[ 1 ];
+        this.idxWeight = 1;
+        break;
+      case ( resultado > 24 && resultado <= 29 ):
+        this.selecWeight = this.typeWeight[ 2 ];
+        this.idxWeight = 2;
+        break;
+      case ( resultado > 29 && resultado <= 39 ):
+        this.selecWeight = this.typeWeight[ 3 ];
+        this.idxWeight = 3;
+        break;
+      case ( resultado > 39 ):
+        this.selecWeight = this.typeWeight[ 4 ];
+        this.idxWeight = 4;
+        break;
+    }
     isNaN( resultado ) || resultado === Infinity
       ? this.bmi = 0
       : this.bmi = resultado;
