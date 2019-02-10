9894a04421c19b2e676002e1539ee01aae68d774
Jorge <jorgecampoy81@gmail.com>
2019-02-10 18:28:17 +0100

    Added button for man and woman; Added images of each typeWeight; Added button for typeWeight

--- src/app/bmi/bmi.page.ts
+++ src/app/bmi/bmi.page.ts
@@ -10,10 +10,27 @@ export class BmiPage implements OnInit {
   height: number;
   weight: number;
   bmi = 0;
+  sex = [ 'Man', 'Woman' ];
+  selecSex = this.sex[ 0 ];
+  typeWeight = [ 'slim', 'fit', 'normal', 'fat', 'fatest' ];
+  idxWeight = 0;
+  selecWeight = this.typeWeight[ this.idxWeight ];
 
   constructor () { }
 
   ngOnInit() { }
+
+  changeSex() {
+    this.selecSex === this.sex[ 0 ]
+      ? this.selecSex = this.sex[ 1 ]
+      : this.selecSex = this.sex[ 0 ];
+  }
+
+  changeImg() {
+    this.idxWeight++;
+    if ( this.idxWeight > 4 ) { this.idxWeight = 0; }
+    this.selecWeight = this.typeWeight[ this.idxWeight ];
+  }
 
   calcBMI() {
     const resultado = this.weight / ( ( this.height / 100 ) * ( this.height / 100 ) );
