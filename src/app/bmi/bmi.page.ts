4c3a99641461181bb13a28f513c163d347bba1f7
Jorge <jorgecampoy81@gmail.com>
2019-02-12 22:14:59 +0100

    Ajusted image Actually we cant choose the sex so that are commented

--- src/app/bmi/bmi.page.ts
+++ src/app/bmi/bmi.page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-export enum Sex { 'Man', 'Woman' }
-export enum weightMeter { 'slim', 'fit', 'normal', 'fat', 'fatest' }
-export enum colorWeight { 'aqua', 'lime', 'yellow', 'orange', 'red' }
+// export enum Sex { 'Man', 'Woman' }
+// export enum weightMeter { 'slim', 'fit', 'normal', 'fat', 'fatest' }
+// export enum colorWeight { 'aqua', 'lime', 'yellow', 'orange', 'red' }
 @Component( {
   selector: 'app-bmi',
   templateUrl: './bmi.page.html',
@@ -14,42 +14,35 @@ export class BmiPage implements OnInit {
   private weight: number;
   public bmiResult = 0;
 
-  public selectedSex: any = Sex[ 0 ]; // 'man'
-  public selectedWeight: any; // 'slim'
-  public colorAcordingWeight: any; // 'aqua'
+  public adjustImgLeft = 0;
 
   constructor () { }
 
   ngOnInit() { }
 
-  changeSex(): void {
-    this.selectedSex === Sex[ 0 ]
-      ? this.selectedSex = Sex[ 1 ]
-      : this.selectedSex = Sex[ 0 ];
-  }
+  // changeSex(): void {
+  //   this.selectedSex === Sex[ 0 ]
+  //     ? this.selectedSex = Sex[ 1 ]
+  //     : this.selectedSex = Sex[ 0 ];
+  // }
 
   calcBMI(): void {
     const result: number = this.weight / ( ( this.height / 100 ) * ( this.height / 100 ) );
     switch ( true ) {
-      case ( result <= 18 ):
-        this.selectedWeight = weightMeter[ 0 ];
-        this.colorAcordingWeight = colorWeight[ 0 ];
+      case ( result <= 18.5 ):
+        this.adjustImgLeft = -5;
         break;
-      case ( result > 18 && result <= 24 ):
-        this.selectedWeight = weightMeter[ 1 ];
-        this.colorAcordingWeight = colorWeight[ 1 ];
+      case ( result > 18.5 && result <= 24.9 ):
+        this.adjustImgLeft = -254;
         break;
-      case ( result > 24 && result <= 29 ):
-        this.selectedWeight = weightMeter[ 2 ];
-        this.colorAcordingWeight = colorWeight[ 2 ];
+      case ( result > 24.9 && result <= 29.9 ):
+        this.adjustImgLeft = -508;
         break;
-      case ( result > 29 && result <= 39 ):
-        this.selectedWeight = weightMeter[ 3 ];
-        this.colorAcordingWeight = colorWeight[ 3 ];
+      case ( result > 29.9 && result <= 34.9 ):
+        this.adjustImgLeft = -762;
         break;
-      case ( result > 39 ):
-        this.selectedWeight = weightMeter[ 4 ];
-        this.colorAcordingWeight = colorWeight[ 4 ];
+      case ( result > 34.9 ):
+        this.adjustImgLeft = -1016;
         break;
     }
     isNaN( result ) || result === Infinity
