be7f62c11f90cf38c29da94808e31fdb0e55a928
Carlos Torres García <togacharls@gmail.com>
2019-03-05 20:08:16 +0100

    code review

--- src/app/bmi/bmi.page.spec.ts
+++ src/app/bmi/bmi.page.spec.ts
@@ -1,20 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { BmiPage } from './bmi.page';
-import { By } from '@angular/platform-browser';
-import { IonicModule, NavController } from '@ionic/angular';
-import { TranslateModule } from '@ngx-translate/core';
-import { FormsModule } from '@angular/forms';
-import { RouterTestingModule } from '@angular/router/testing';
-import { Genre } from "./enums/bmi.genre.enum";
+import {BmiPage} from './bmi.page';
+import {By} from '@angular/platform-browser';
+import {IonicModule, NavController} from '@ionic/angular';
+import {TranslateModule} from '@ngx-translate/core';
+import {FormsModule} from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing';
+import {Genre} from './enums/bmi.genre.enum';
+import {DebugElement} from '@angular/core';
+import {BmiRange} from './enums/bmi.bmi-range.enum';
 
-describe( 'BmiPage html elements', () => {
+describe('BmiPage html elements', () => {
   let component: BmiPage;
   let fixture: ComponentFixture<BmiPage>;
 
-  beforeEach( () => {
-    TestBed.configureTestingModule( {
-      declarations: [ BmiPage ],
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [BmiPage],
       imports: [
         IonicModule,
         TranslateModule.forRoot(),
@@ -22,170 +24,133 @@ describe( 'BmiPage html elements', () => {
         RouterTestingModule
       ],
       providers: [
-        { provide: NavController, useValue: null }
+        {provide: NavController, useValue: null}
       ]
-    } );
-  } );
+    });
+  });
 
-  beforeEach( () => {
-    fixture = TestBed.createComponent( BmiPage );
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BmiPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  } );
+  });
 
 
-  it( 'should create a title and is translatable ', () => {
-    const element: any = fixture.debugElement.query( By.css( 'ion-title' ) ).nativeElement;
+  it('Should create a title and is translatable ', () => {
+    const element: any = fixture.debugElement.query(By.css('ion-title')).nativeElement;
 
-    expect( element ).toBeTruthy();
-    expect( element.translate ).toBeTruthy();
-    expect( element.innerHTML ).toContain( 'BMI.SUBTITLE' );
-  } );
+    expect(element).toBeTruthy();
+    expect(element.translate).toBeTruthy();
+    expect(element.innerHTML).toContain('BMI.SUBTITLE');
+  });
 
-  it( 'should create a back button and is not hidden', () => {
-    const element: HTMLIonBackButtonElement = fixture.debugElement.query( By.css( 'ion-back-button' ) ).nativeElement;
+  it('Should have a back button', () => {
+    const element: HTMLIonBackButtonElement = fixture.debugElement.query(By.css('ion-back-button')).nativeElement;
 
-    expect( element ).toBeTruthy();
-    expect( element.hidden ).toBeFalsy();
-  } );
+    expect(element).toBeTruthy();
+  });
 
-  it( 'All the labels should exist and are initialized with the following contents:', () => {
-    const element: Array<any> = fixture.debugElement.queryAll( By.css( 'ion-label' ) );
+  it('All the labels should be initialized', () => {
+    const element: Array<any> = fixture.debugElement.queryAll(By.css('ion-label'));
 
-    expect( element[ 0 ].nativeElement.innerHTML ).toContain( 'BMI.HEIGHT (cm)' );
-    expect( element[ 1 ].nativeElement.innerHTML ).toContain( 'BMI.WEIGHT (kg)' );
-    expect( element[ 2 ].nativeElement.innerHTML ).toContain( 'BMI.WOMAN' );
-    expect( element[ 3 ].nativeElement.innerHTML ).toContain( 'BMI.MAN' );
-    expect( element[ 4 ].nativeElement.innerHTML ).toContain( 'BMI.RESULT: 0' );
-    expect( element[ 5 ].nativeElement.innerHTML ).toContain( 'BMI.CALC' );
-    expect( element ).toBeTruthy();
-  } );
+    expect(element[0].nativeElement.innerHTML).toContain('BMI.HEIGHT (cm)');
+    expect(element[1].nativeElement.innerHTML).toContain('BMI.WEIGHT (kg)');
+    expect(element[2].nativeElement.innerHTML).toContain('BMI.WOMAN');
+    expect(element[3].nativeElement.innerHTML).toContain('BMI.MAN');
+    expect(element[4].nativeElement.innerHTML).toContain('BMI.RESULT: 0');
+    expect(element[5].nativeElement.innerHTML).toContain('BMI.CALC');
+    expect(element).toBeTruthy();
+  });
 
-  it( 'bmiCalc() should return the correct value ', () => {
-    const elButton: any = fixture.debugElement.query( By.css( 'ion-button' ) );
+  describe('bmiCalc()', () => {
+    it('Should return the correct value ', () => {
+      const elButton: DebugElement = fixture.debugElement.query(By.css('ion-button'));
 
-    component.height = 180;
-    component.weight = 80;
-    elButton.triggerEventHandler( 'click', null );
-    expect( component.bmiResult ).toBe( 24.691358024691358 );
-  } );
+      component.height = 180;
+      component.weight = 80;
+      elButton.triggerEventHandler('click', null);
+      expect(component.bmiResult).toBeLessThan(BmiRange.OVERWEIGHT);
+    });
+  });
 
-  it( 'getSilhouetteImg() should return the correct path of bmiImage', () => {
-    const elSelect: any[] = fixture.debugElement.queryAll( By.css( 'ion-radio' ) );
+  describe('getSilhouetteImg()', () => {
+    it('Should return the correct path of bmiImage', () => {
+      const elSelect: Array<DebugElement> = fixture.debugElement.queryAll(By.css('ion-radio'));
 
-    component.genre = Genre.WOMAN;
-    expect( elSelect[ 0 ].nativeElement.value ).toBe( 'woman' );
-    expect( component[ 'getSilhouetteImg' ]() ).toBe( `../../assets/img/bmi/bmi_${ elSelect[ 0 ].nativeElement.value }_silhouettes.png` );
-    component.genre = Genre.MAN;
-    expect( elSelect[ 1 ].nativeElement.value ).toBe( 'man' );
-    expect( component[ 'getSilhouetteImg' ]() ).toBe( `../../assets/img/bmi/bmi_${ elSelect[ 1 ].nativeElement.value }_silhouettes.png` );
-  } );
+      component.genre = Genre.WOMAN;
+      expect(elSelect[0].nativeElement.value).toBe('woman');
+      expect(component['getSilhouetteImg']()).toBe(`../../assets/img/bmi/bmi_${ elSelect[0].nativeElement.value }_silhouettes.png`);
+      component.genre = Genre.MAN;
+      expect(elSelect[1].nativeElement.value).toBe('man');
+      expect(component['getSilhouetteImg']()).toBe(`../../assets/img/bmi/bmi_${ elSelect[1].nativeElement.value }_silhouettes.png`);
+    });
+  });
 
-  it( 'disableCalcBMI() should disable tbe button of cbmiCalc', () => {
-    component.height = undefined;
-    component.weight = undefined;
-    expect( component.disableCalcBMI() ).toBeTruthy();
-    component.height = 180;
-    component.weight = 80;
-    expect( component.disableCalcBMI() ).toBeFalsy();
-  } );
+  describe('disableCalcBMI()', () => {
+    it(' should disable tbe button of cbmiCalc', () => {
+      component.height = undefined;
+      component.weight = undefined;
+      expect(component.disableCalcBMI()).toBeTruthy();
+      component.height = 180;
+      component.weight = 80;
+      expect(component.disableCalcBMI()).toBeFalsy();
+    });
+  });
 
-} );
+  describe('updateChoosenSilhouette()', () => {
+    let elButton: DebugElement;
+    let positionSillhouette: HTMLElement;
 
+    beforeEach(() => {
+      component.height = 0;
+      component.weight = 0;
+      elButton = fixture.debugElement.query(By.css('ion-button'));
+      positionSillhouette = fixture.debugElement.query(By.css('div.choosen-silhouette')).nativeElement;
+    });
 
+    it('BmiResult.SLIM should place correctly the div.chosen-silhouette', () => {
+      component.height = 180;
+      component.weight = 50;
+      elButton.triggerEventHandler('click', null);
+      expect(positionSillhouette.style.marginLeft).toBe('0%');
+      expect(positionSillhouette.style.right).toBe('initial');
+      expect(positionSillhouette.style.width).toBe('16%');
+    });
 
-describe( 'updateChoosenSilhouette() should modify the position correctly', () => {
-  let component: BmiPage;
-  let fixture: ComponentFixture<BmiPage>;
-  let elButton: any;
-  let positionSillhouette: any;
+    it('BmiResult.FIT should place correctly the div.chosen-silhouette', () => {
+      component.height = 180;
+      component.weight = 70;
+      elButton.triggerEventHandler('click', null);
+      expect(positionSillhouette.style.marginLeft).toBe('18%');
+      expect(positionSillhouette.style.right).toBe('initial');
+      expect(positionSillhouette.style.width).toBe('18%');
+    });
 
-  beforeEach( () => {
-    TestBed.configureTestingModule( {
-      declarations: [ BmiPage ],
-      imports: [
-        IonicModule,
-        TranslateModule.forRoot(),
-        FormsModule,
-        RouterTestingModule
-      ],
-      providers: [
-        { provide: NavController, useValue: null }
-      ]
-    } );
-  } );
+    it('BmiResult.OVERWEIGHT should place correctly the div.chosen-silhouette', () => {
+      component.height = 180;
+      component.weight = 90;
+      elButton.triggerEventHandler('click', null);
+      expect(positionSillhouette.style.marginLeft).toBe('36%');
+      expect(positionSillhouette.style.right).toBe('initial');
+      expect(positionSillhouette.style.width).toBe('18%');
+    });
 
-  beforeEach( () => {
-    fixture = TestBed.createComponent( BmiPage );
-    component = fixture.componentInstance;
-    component.height = 0;
-    component.weight = 0;
-    fixture.detectChanges();
-    elButton = fixture.debugElement.query( By.css( 'ion-button' ) );
-    positionSillhouette = fixture.debugElement.query( By.css( 'div.choosen-silhouette' ) ).nativeElement;
-  } );
+    it('BmiResult.OBESE should place correctly the div.chosen-silhouette', () => {
+      component.height = 180;
+      component.weight = 100;
+      elButton.triggerEventHandler('click', null);
+      expect(positionSillhouette.style.marginLeft).toBe('0%');
+      expect(positionSillhouette.style.right).toBe('24%');
+      expect(positionSillhouette.style.width).toBe('17%');
+    });
 
-  // FROM LOW TO HIGHT BMIRESULT
-  it( 'BmiResult.SLIM should place correctly the div.chosen-silhouette', () => {
-    component.height = 180;
-    component.weight = 50;
-    elButton.triggerEventHandler( 'click', null );
-    expect( positionSillhouette.style.marginLeft ).toBe( '0%' );
-    expect( positionSillhouette.style.right ).toBe( 'initial' );
-    expect( positionSillhouette.style.width ).toBe( '16%' );
-  } );
-
-  it( 'BmiResult.FIT should place correctly the div.chosen-silhouette', () => {
-    component.height = 180;
-    component.weight = 70;
-    elButton.triggerEventHandler( 'click', null );
-    expect( positionSillhouette.style.marginLeft ).toBe( '18%' );
-    expect( positionSillhouette.style.right ).toBe( 'initial' );
-    expect( positionSillhouette.style.width ).toBe( '18%' );
-  } );
-
-  it( 'BmiResult.OVERWEIGHT should place correctly the div.chosen-silhouette', () => {
-    component.height = 180;
-    component.weight = 90;
-    elButton.triggerEventHandler( 'click', null );
-    expect( positionSillhouette.style.marginLeft ).toBe( '36%' );
-    expect( positionSillhouette.style.right ).toBe( 'initial' );
-    expect( positionSillhouette.style.width ).toBe( '18%' );
-  } );
-  it( 'BmiResult.OBESE should place correctly the div.chosen-silhouette', () => {
-    component.height = 180;
-    component.weight = 100;
-    elButton.triggerEventHandler( 'click', null );
-    expect( positionSillhouette.style.marginLeft ).toBe( '0%' );
-    expect( positionSillhouette.style.right ).toBe( '24%' );
-    expect( positionSillhouette.style.width ).toBe( '17%' );
-  } );
-
-  it( 'BmiResult.MORBID should place correctly the div.chosen-silhouette', () => {
-    component.height = 180;
-    component.weight = 120;
-    elButton.triggerEventHandler( 'click', null );
-    expect( positionSillhouette.style.marginLeft ).toBe( '0%' );
-    expect( positionSillhouette.style.right ).toBe( '17px' );
-    expect( positionSillhouette.style.width ).toBe( '19%' );
-  } );
-
-  // FROM HIGHT TO LOW BMIRESULT
-  it( 'div.chosen-silhouette should be placed correctly from position MORBID to SLIM ', () => {
-    // MORBID POSITION
-    component.height = 180;
-    component.weight = 120;
-    elButton.triggerEventHandler( 'click', null );
-    expect( positionSillhouette.style.marginLeft ).toBe( '0%' );
-    expect( positionSillhouette.style.right ).toBe( '17px' );
-    expect( positionSillhouette.style.width ).toBe( '19%' );
-    // SLIM POSITION
-    component.height = 180;
-    component.weight = 50;
-    elButton.triggerEventHandler( 'click', null );
-    expect( positionSillhouette.style.marginLeft ).toBe( '0%' );
-    expect( positionSillhouette.style.right ).toBe( 'initial' );
-    expect( positionSillhouette.style.width ).toBe( '16%' );
-  } );
-
-} );
+    it('BmiResult.MORBID should place correctly the div.chosen-silhouette', () => {
+      component.height = 180;
+      component.weight = 120;
+      elButton.triggerEventHandler('click', null);
+      expect(positionSillhouette.style.marginLeft).toBe('0%');
+      expect(positionSillhouette.style.right).toBe('17px');
+      expect(positionSillhouette.style.width).toBe('19%');
+    });
+  });
+});
--- src/app/bmi/bmi.page.ts
+++ src/app/bmi/bmi.page.ts
@@ -1,13 +1,12 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
-import { Genre } from "./enums/bmi.genre.enum";
-import { BmiRange } from "./enums/bmi.bmi-range.enum";
+import {Component, ElementRef, OnInit} from '@angular/core';
+import {Genre} from './enums/bmi.genre.enum';
+import {BmiRange} from './enums/bmi.bmi-range.enum';
 
-// export enum colorWeight { 'aqua', 'lime', 'yellow', 'orange', 'red' }
-@Component( {
+@Component({
   selector: 'app-bmi',
   templateUrl: './bmi.page.html',
-  styleUrls: [ './bmi.page.scss' ],
-} )
+  styleUrls: ['./bmi.page.scss'],
+})
 export class BmiPage implements OnInit {
 
   public height: number;
@@ -15,8 +14,10 @@ export class BmiPage implements OnInit {
   public genre: Genre;
 
   public bmiResult = 0;
-  protected readonly GenreEnum = Genre;
-  constructor ( private elRef: ElementRef ) { }
+  public readonly GenreEnum = Genre;
+
+  constructor(private elRef: ElementRef) {
+  }
 
   ngOnInit() {
     this.genre = Genre.WOMAN;
@@ -31,46 +32,39 @@ export class BmiPage implements OnInit {
     return !this.height || !this.weight;
   }
 
-  protected getSilhouetteImg(): string {
+  getSilhouetteImg(): string {
     return '../../assets/img/bmi/bmi_' + this.genre + '_silhouettes.png';
   }
 
   private calcBMI(): void {
-    const result: number = this.weight / ( ( this.height / 100 ) * ( this.height / 100 ) );
-    isNaN( result ) || result === Infinity
+    const result: number = this.weight / ((this.height / 100) * (this.height / 100));
+    isNaN(result) || result === Infinity
       ? this.bmiResult = 0
       : this.bmiResult = result;
   }
 
   private updateChoosenSilhouette(): void {
-    const choosenSilhouette = this.elRef.nativeElement.querySelector( 'div.choosen-silhouette' );
-
-    switch ( true ) {
-      case this.bmiResult <= BmiRange.SLIM:
-        choosenSilhouette.style.marginLeft = '0%';
-        choosenSilhouette.style.right = 'initial';
-        choosenSilhouette.style.width = '16%';
-        break;
-      case this.bmiResult <= BmiRange.FIT:
-        choosenSilhouette.style.marginLeft = '18%';
-        choosenSilhouette.style.right = 'initial';
-        choosenSilhouette.style.width = '18%';
-        break;
-      case this.bmiResult <= BmiRange.OVERWEIGHT:
-        choosenSilhouette.style.marginLeft = '36%';
-        choosenSilhouette.style.right = 'initial';
-        choosenSilhouette.style.width = '18%';
-        break;
-      case this.bmiResult <= BmiRange.OBESE:
-        choosenSilhouette.style.marginLeft = '0%';
-        choosenSilhouette.style.right = '24%';
-        choosenSilhouette.style.width = '17%';
-        break;
-      case this.bmiResult <= BmiRange.MORBID:
-        choosenSilhouette.style.marginLeft = '0%';
-        choosenSilhouette.style.right = '17px';
-        choosenSilhouette.style.width = '19%';
-        break;
+    const choosenSilhouette = this.elRef.nativeElement.querySelector('div.choosen-silhouette');
+    if (this.bmiResult <= BmiRange.SLIM) {
+      choosenSilhouette.style.marginLeft = '0%';
+      choosenSilhouette.style.right = 'initial';
+      choosenSilhouette.style.width = '16%';
+    } else if (this.bmiResult <= BmiRange.FIT) {
+      choosenSilhouette.style.marginLeft = '18%';
+      choosenSilhouette.style.right = 'initial';
+      choosenSilhouette.style.width = '18%';
+    } else if (this.bmiResult <= BmiRange.OVERWEIGHT) {
+      choosenSilhouette.style.marginLeft = '36%';
+      choosenSilhouette.style.right = 'initial';
+      choosenSilhouette.style.width = '18%';
+    } else if (this.bmiResult <= BmiRange.OBESE) {
+      choosenSilhouette.style.marginLeft = '0%';
+      choosenSilhouette.style.right = '24%';
+      choosenSilhouette.style.width = '17%';
+    } else {
+      choosenSilhouette.style.marginLeft = '0%';
+      choosenSilhouette.style.right = '17px';
+      choosenSilhouette.style.width = '19%';
     }
   }
 }
