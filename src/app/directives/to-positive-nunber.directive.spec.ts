a208eb24581478ecd3234b08893fa9b27d1226e0
Carlos Torres García <togacharls@gmail.com>
2019-03-23 16:29:34 +0100

    fix: aot compilation and passing tests

--- src/app/directives/to-positive-nunber.directive.spec.ts
+++ src/app/directives/to-positive-nunber.directive.spec.ts
@@ -2,38 +2,38 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { By } from '@angular/platform-browser';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { DebugElement } from '@angular/core';
+import {Component, DebugElement} from '@angular/core';
 import { ToPositiveNumberDirective } from './to-positive-number.directive';
-import { ToPositiveNumberDirectiveTest } from './to-positive-number.directive-test';
+
+@Component({
+  template: `<input appToPositiveNumber min="1" type="number">`
+})
+class TestComponentWithToPositiveNumberDirective{}
 
 describe( 'ToPositiveNumberDirective', () => {
 // let keyboardValue: any;
-let component: ToPositiveNumberDirectiveTest;
-let fixture: ComponentFixture<ToPositiveNumberDirectiveTest>;
-let inputEl: DebugElement;
+  let component: TestComponentWithToPositiveNumberDirective;
+  let fixture: ComponentFixture<TestComponentWithToPositiveNumberDirective>;
+  let inputEl: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        IonicModule,
+        FormsModule ],
+      declarations: [ TestComponentWithToPositiveNumberDirective, ToPositiveNumberDirective ]
+    });
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponentWithToPositiveNumberDirective);
+    component = fixture.componentInstance;
+    inputEl = fixture.debugElement.query(By.css('input'));
+  });
 
 
-beforeEach(() => {
-  TestBed.configureTestingModule({
-    imports: [
-      IonicModule,
-      FormsModule ],
-    declarations: [ ToPositiveNumberDirectiveTest, ToPositiveNumberDirective ]
+  it( 'should create an instance', () => {
+    fixture.detectChanges();
+    inputEl.triggerEventHandler('keydown', {keyCode: 104});
   });
 });
-
-beforeEach(() => {
-  fixture = TestBed.createComponent(ToPositiveNumberDirectiveTest);
-  component = fixture.componentInstance;
-  inputEl = fixture.debugElement.query(By.css('input'));
-});
-
-
-it( 'should create an instance', () => {
-  fixture.detectChanges();
-  inputEl.triggerEventHandler('keydown', {keyCode: 104});
-    
-  console.log(inputEl);
-  } );
-
-} );
