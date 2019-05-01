68edc581a2aecab17c708eb85e8b95420272d8f5
Carlos Torres García <togacharls@gmail.com>
2019-05-01 08:53:15 +0200

    chore(ngrx): added initStore action to bmi genre reducer

--- src/app/bmi/bmi.reducers.ts
+++ src/app/bmi/bmi.reducers.ts
@@ -1,7 +1,20 @@
 import { Genre } from './enums/bmi.genre.enum';
+import { getInitialState } from '../app.state';
 import * as BMI_ACTIONS from './bmi.actions';
+import * as APP_ACTIONS from '../app.actions';
 
-export function genre(state: Genre, action: BMI_ACTIONS.ToggleGenre): Genre {
+const toggleGenreReducer = (action: BMI_ACTIONS.ToggleGenre): Genre => action.chosenGenre;
 
-    return action.chosenGenre;
+export function genre(
+  state: Genre,
+  action: BMI_ACTIONS.ToggleGenre | APP_ACTIONS.InitStore | any
+): Genre {
+  switch (action.type) {
+    case BMI_ACTIONS.TYPES.toggleGenre:
+      return toggleGenreReducer(action);
+    case APP_ACTIONS.TYPES.initStore:
+      return getInitialState().genre;
+    default:
+      throw new Error(`Non registered action: ${action.type}`);
+  }
 }
