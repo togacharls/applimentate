2e2e0b08d4d0b4983ad836ebb99ee9b3e0d293bb
Carlos Torres García <togacharls@gmail.com>
2019-05-01 08:36:09 +0200

    refactor: appstate in another file

--- src/app/app.reducers.ts
+++ src/app/app.reducers.ts
@@ -1,10 +1,6 @@
 import { ActionReducerMap } from '@ngrx/store';
-import { Genre } from './bmi/enums/bmi.genre.enum';
+import { AppState } from './app.state';
 import * as BMI_REDUCERS from './bmi/bmi.reducers';
-
-export interface AppState {
-    genre: Genre;
-}
 
 export const appReducers: ActionReducerMap<AppState> = {
     genre: BMI_REDUCERS.genre
---
+++ src/app/app.state.ts
@@ -0,0 +1,5 @@
+import { Genre } from './bmi/enums/bmi.genre.enum';
+
+export interface AppState {
+  genre: Genre;
+}
